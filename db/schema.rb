7b082a5b71438b33d9750483dcc101ee89648683
arunr <arunr.rubydev@gmail.com>
2017-09-13 13:25:00 +0530

    changes in schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170907132306) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bags", force: :cascade do |t|
   end
 
